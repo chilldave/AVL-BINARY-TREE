3a398770e4677a7f8f624bfc7659cbfd0bd58056
david <supercellcamey1@gmail.com>
2024-05-26 13:47:03 -0600

    Adding Format to AVL tree [2.2]

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
  AVL-BINARY-TREE 
 Welcome to my humble final project, This project is being developed in C++ Language.
-The purpuse of this project is to create and program a AVL-TREE
+The purpuse of this project is to create and program a AVL-TREE.
+
+WARNING: This programa has been developed for Windows.
 
 Prerequisite:
 - install gcc compiler for c/c++ - [11.4.0  or the newest version]
 - install vscode or any other source code editor
+- install windows.h library
 
 Steps to compiling and running:
 
@@ -12,7 +15,7 @@ Steps to compiling and running:
 - clone this repository in your laptop or PC with the follow command:
   git clone https://github.com/chilldave/AVL-BINARY-TREE.git
 - once the project had been cloned, it needs to be compiled, use the follow command:
-  g++ <name.cpp> -o <output.exe>   (for linux it's not necessary to include extension)
+  g++ <name.cpp> -o <output.exe>
 
 Features of AVL Tree
 - Insert a node in BST
@@ -20,3 +23,4 @@ Features of AVL Tree
 - Delete a node in BST
 - Display the BST
 - Exit
+
--- avl-binary-tree.cpp
+++ avl-binary-tree.cpp
@@ -5,12 +5,6 @@
 #include <conio.h>
 #include <windows.h>
 
-#define WIDTH 70
-#define WEIGHT 35
-
-
-
-
 void SetConsoleWindowSize(int width, int height) {
     HWND console = GetConsoleWindow();
     RECT r;
@@ -18,15 +12,12 @@ void SetConsoleWindowSize(int width, int height) {
     MoveWindow(console, r.left, r.top, width, height, TRUE);
 }
 
-
 void gotoxy(int x, int y){
 
     COORD coord;
     coord.X = x;
     coord.Y = y;
-
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-
 }
 
 
@@ -37,21 +28,17 @@ void setConsoleSize(int cols, int rows){
         return; 
 
     SMALL_RECT r;
-
     r.Left = 0;
     r.Top = 0;
     r.Right = cols -1;
     r.Bottom = rows -1;
     SetConsoleWindowInfo(hOut, TRUE, &r);
-
     COORD c;
-
     c.X = cols;
     c.Y = rows;
-
     SetConsoleScreenBufferSize(hOut, c);
-
 }
+//We have the Node structure 
 typedef struct Node {
     int number;
     struct Node *left;
@@ -60,18 +47,19 @@ typedef struct Node {
 } stNode;
 
 // Global variables, it will help us out to avoid
-bool flag = false;
-int counter;
-stNode *ptrNew, *ptrAux, *mainRoot = nullptr;
+bool flag = false; stNode *ptrNew, *ptrAux, *mainRoot = nullptr; int i = 10;
 
+// menu and getRightInput is really important to manage the input
 int getRightInput();
 void menu();
 
-// Functions to create a new node, insert and show the AVL TREE
+/*   Functions to create a new node, insert, search, show, delete  Nodo
+     Thre is another function named getNode_Right_SI, it means we are going to get the Node+Right -> left->subtree
+*/
 stNode *createNode(int number);
 void insertNode(stNode *&root, int number);
 void searchNode(stNode *root, int number);
-void showTree(stNode *root);
+void showTree(stNode *root, int x, int y, int offset);
 stNode *getNode_Right_SI(stNode *root);
 void deleteNode(stNode *&root, int number);
 
@@ -85,11 +73,11 @@ stNode *getDoubleRightRotate(stNode *&root);
 stNode *getDoubleLeftRotate(stNode *&root);
 stNode *balanceTree(stNode *&root);
 
+
 int main(int argc, char const *argv[]) {
     system("cls");
-    setConsoleSize(100,30);
-    // SetConsoleWindowSize(400,600);
-    stNode *ptrRoot = nullptr;  
+    SetConsoleWindowSize(1650,600);
+    setConsoleSize(180,35);
     menu();
     return 0;
 }
@@ -97,9 +85,13 @@ int main(int argc, char const *argv[]) {
 // this function will help us out to get the right input from the user
 int getRightInput() {
     int option;
-
+    /*
+        We are going to ask a number, cin.fail() helps to verify whether the input was certainly well or not 
+        if cin.fail() returns true, we are going to show a message, we are going to clear the cin input
+        cin.ignore -> reject the wrong input
+        but it returns false, the code will end the while loop
+    */
     while (true) {
-        // system("cls");
         gotoxy(20,7); std::cout << "                                         ";
         gotoxy(20,7); std::cout << "Ingrese Un Numero: ";
         std::cin >> option;
@@ -107,8 +99,7 @@ int getRightInput() {
         if (std::cin.fail()) {
             gotoxy(20,9); std::cerr << "Dato Invalido. Por Favor Ingrese Un Dato Valido.";
             std::cin.clear();
-            // we are going to ignore the rest of the characters in the buffer
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // we are going to ignore the rest of the characters in the buffer
             std::cin.get();
             gotoxy(20,7); std::cout << "                                             ";
             gotoxy(20,9); std::cout << "                                                                  "; 
@@ -120,11 +111,10 @@ int getRightInput() {
 }
 
 void menu() {
-    int option;
-    stNode *root = nullptr;
+    int option; stNode *root = nullptr; //define local variables
+
     while (true) {
         system("cls");
-
         gotoxy(35,5); std::cout << "ARBOL BINARIO DE BUSQUEDA (AVL)";
         gotoxy(25,8); std::cout << "[1]. Insertar Nodo.";
         gotoxy(25,9); std::cout << "[2]. Buscar Nodo. ";
@@ -140,20 +130,21 @@ void menu() {
                 gotoxy(24,19); throw std::invalid_argument("Dato Invalido. Por Favor Ingrese Un Dato Valido.");
             }
             switch (option) {
-                case 1:
+                case 1: //Insert a Node
                     system("cls");
                     gotoxy(33,5); std::cout << "INSERTAR UN NODO EN EL ARBOL AVL.";
+                    gotoxy(90,5); std::cout << "mainRoot -> " << mainRoot;
                     option = getRightInput();
                     insertNode(root, option);
-                    gotoxy(33,24); std::cout<<"mainRoot: " << mainRoot->number;
+                    gotoxy(24,22); std::cout << "Presione enter para continuar...";
                     std::cin.get();
                     std::cin.get();
                     break;
-                case 2:
+                case 2: // Search a Node and we are going to verify if the tree is empty or not
                     system("cls");
                     gotoxy(33,5); std::cout << "BUSCAR UN NODO EN EL ARBOL AVL";
                     if (root == nullptr) {
-                        gotoxy(20,8);std::cout << "El Arbol AVL Esta Vacio. Inserte Un Nodo";
+                        gotoxy(20,8);std::cout << "El Arbol AVL Esta Vacio.";
                     } else {
                         option = getRightInput();
                         flag = false;
@@ -165,7 +156,7 @@ void menu() {
                     std::cin.get();
                     std::cin.get();
                     break;
-                case 3:
+                case 3: // Delete a nodo and verify if the tree is empty or not
                     system("cls");
                     gotoxy(33,5); std::cout << "ELIMINAR NODO EN EL ARBOL AVL";
                     if (root == nullptr) {
@@ -183,18 +174,24 @@ void menu() {
                     std::cin.get();
                     std::cin.get();
                     break;
-                case 4:
+                case 4: // Show AVL Tree with Format
                     system("cls");
-                    gotoxy(33,5); std::cout << "VISUALIZAR EL ARBOL AVL";
+                    gotoxy(40,5); std::cout << "VISUALIZAR EL ARBOL AVL";
                     if (root == nullptr) {
-                        gotoxy(20,8); std::cout << "El Arbol AVL Esta Vacio.";
+                        gotoxy(171,8); std::cout << "*";
+
+                        gotoxy(0,8); std::cout << "*";
+                        gotoxy(25,8); std::cout << "El Arbol AVL Esta Vacio.";
                     } else {
-                        showTree(root);
+                        gotoxy(179,8); std::cout << "*";
+                        gotoxy(0,8); std::cout << "*";
+                        showTree(root,87,8,44); 
+                        gotoxy(24,22); std::cout << "Presione enter para continuar...";
                     }
                     std::cin.get();
                     std::cin.get();
                     break;
-                case 5:
+                case 5: //End the while loop
                     system("cls");
                     exit(0);
                     break;
@@ -206,11 +203,10 @@ void menu() {
                     break;  
             }
         } catch (const std::invalid_argument &e) {
-            std::cerr << e.what() << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            // std::cout << "Presione Enter para continuar...";
-            std::cin.get(); // Esperar a que el usuario presione Enter
+            std::cerr << e.what() << std::endl; //It will display a error message 
+            std::cin.clear(); // it will clear the input 
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // it will ignore and reject the information 
+            gotoxy(72,19);std::cin.get(); // Esperar a que el usuario presione Enter
         }
     }
 }
@@ -229,23 +225,18 @@ stNode *createNode(int number) {
 void insertNode(stNode *&root, int number) {
     // if the root is null, we are going to create a new node.
     if (root == nullptr) {
-        // we are going to create a new node
-        root = createNode(number);
-        // we are going to set the mainRoot with the root, with purpose to have a reference to the root and not include the same value at root
-        if (mainRoot == nullptr) {
+        root = createNode(number);   // we are going to create a new node
+        if (mainRoot == nullptr) {   // we are going to set the mainRoot with the root, with purpose to have a reference to the root and not include the same value at root
             mainRoot = root;
         }
         gotoxy(20,10); std::cout << "Nodo Insertado.";
-        // we are going to update the height of the root
-        updateHeight(root);
-        root = balanceTree(root);
-        counter++;
-        mainRoot = root;
+        updateHeight(root);           // we are going to update the height of the root
+        root = balanceTree(root);     // we are going to balance the tree   
         return;
     }
-
+    // if the nuber is the same as root, it will be rejected 
     if (number == mainRoot->number) {
-        gotoxy(20,9); std::cout << "El Nodo Es Igual Al Valor De La Raiz, Nodo Rechazado.";
+        gotoxy(20,9); std::cout << "El Numero Es Igual Al Valor De La Raiz, Nodo Rechazado.";
         return;
     }
 
@@ -253,29 +244,26 @@ void insertNode(stNode *&root, int number) {
     //  si es mayor se va a insertar a la derecha
     (number <= root->number) ? insertNode(root->left, number) : insertNode(root->right, number);
     updateHeight(root);
-    mainRoot = root;
     root = balanceTree(root);
+    mainRoot =  root;  // we are going to update the mainRoot pointer in case the root has change
 }
 
 void searchNode(stNode *root, int number) {
-    if (root == nullptr)
-        return;
-
-    // else
+    if (root == nullptr)  return;
 
     if (root->number == number) {
-        gotoxy(20,10); std::cout << "Nodo Encontrado: " << root->number << " En La Direccion: " << root;
+        gotoxy(20,i); std::cout << "Nodo Encontrado: " << root->number << " En La Direccion: " << root;
         flag = true;
+        i++;
     }
-
-    (number <= root->number) ? searchNode(root->left, number) : searchNode(root->right, number);
+    // (number <= root->number) ? searchNode(root->left, number) : searchNode(root->right, number);
+    searchNode(root->left, number); searchNode(root->right, number);
 }
 
 // This function will help us out to find out where and what is the maximum value of Node more right in left subtree (Node+D: SI)
 stNode *getNode_Right_SI(stNode *root) {
-    while (root->right != nullptr) {
+    while (root->right != nullptr) 
         root = root->right;
-    }
     return root;
 }
 
@@ -285,13 +273,9 @@ void deleteNode(stNode *&root, int number) {
         Case 2:  when the father only has a child
         Case 3:  when the father has two children
     */
-
-    if (root == nullptr) {
-        return;
-    }
+    if (root == nullptr)  return;
 
     // once we already checked the root is not nullptr, we are going to move on to the next step
-
     if (number < root->number) {
         deleteNode(root->left, number);
     } else if (number > root->number) {
@@ -304,61 +288,51 @@ void deleteNode(stNode *&root, int number) {
         if (root->left == nullptr && root->right == nullptr) {
             free(root);
             root = nullptr;
-        }
-
-        // case 2: when the father only has a child
-        else if (root->left == nullptr || root->right == nullptr) {
+        } else if (root->left == nullptr || root->right == nullptr) { // case 2: when the father only has a child
             ptrAux = root;
             root = (root->left == nullptr) ? root->right : root->left;
             free(ptrAux);
-        }
-        // case 3: when the father has two children
-        else {
-            ptrAux = getNode_Right_SI(root->left);
+        } else {   // case 3: when the father has two children
+            ptrAux = getNode_Right_SI(root->left); 
             root->number = ptrAux->number;
             deleteNode(root->left, ptrAux->number);
         }
     }
     updateHeight(root);
     root = balanceTree(root);
-    //we need to update the mainRoot in order to update always the root 
-    mainRoot = root;
+    mainRoot = root; //we need to update the mainRoot in order to update always the root 
 }
 
-void showTree(stNode *root) {
-    if (root != nullptr) {
-        std::cout << "\t" << root->number;
-        showTree(root->left);
-        showTree(root->right);
+void showTree(stNode* root, int x, int y, int offset) {
+    if (root) {
+        gotoxy(x, y);
+        std::cout <<"["<< root->number <<"]";
+        if (root->left) {
+            gotoxy(x - offset / 2, y + 1);
+            std::cout << "* ";
+            showTree(root->left, x - offset, y + 2, offset / 2);
+        }   
+        if (root->right) {
+            gotoxy(x + offset / 2, y + 1);
+            std::cout << "   *";
+            showTree(root->right, (x+1) + offset, y + 2, offset / 2);
+        }
     }
 }
 
-int getHeight(stNode *root) {
+int getHeight(stNode *root) { 
     return (root == nullptr) ? 0 : root->height;
 }
 
 void updateHeight(stNode *&root) {
     if (root != nullptr) {
-        // height(root->left) = 1
-        // height(root->right) = 0
-        // root->height = 2
+        //we are going to update the height in each node
         root->height = 1 + std::max(getHeight(root->left), getHeight(root->right));
     }
 }
-/*
-levels:
-    20 -> height 1
-
-levels:
-    20 -> height 3
-    10 -> height 2
-    5 -> height 1
-
-
-
-*/
 
 int getBalance(stNode *root) {
+    //Balnace the tree, we are going to make sure the result could be -1,0,1
     return (root == nullptr) ? 0 : getHeight(root->left) - getHeight(root->right);
 }
 // this function is the same as we learned in class, we know it as "Left-Left CASE"
@@ -366,8 +340,7 @@ stNode *getRightRotate(stNode *&root) {
     ptrAux = root->left;
     root->left = ptrAux->right;
     ptrAux->right = root;
-    // we are going to update the height of the root
-    updateHeight(root);
+    updateHeight(root);  // we are going to update the height of the root
     updateHeight(ptrAux);
 
     return ptrAux;
@@ -378,9 +351,7 @@ stNode *getLeftRotate(stNode *&root) {
     ptrAux = root->right;
     root->right = ptrAux->left;
     ptrAux->left = root;
-
-    // we are going to update the height of the root
-    updateHeight(root);
+    updateHeight(root); // we are going to update the height of the root
     updateHeight(ptrAux);
 
     return ptrAux;
@@ -397,27 +368,22 @@ stNode *getDoubleLeftRotate(stNode *&root) {
 }
 
 stNode *balanceTree(stNode *&root) {
-    // we are going to get the balance of the root
-    int balance = getBalance(root);
+    int balance = getBalance(root); // we are going to get the balance of the root
 
-    // Left-Left CASE
-    if (balance > 1 && getBalance(root->left) > 0) {
+    if (balance > 1 && getBalance(root->left) > 0) {     // Left-Left CASE
         return getRightRotate(root);
     }
-    // Right-Right CASE
-    if (balance < -1 && getBalance(root->right) < 0) {
+
+    if (balance < -1 && getBalance(root->right) < 0) {    // Right-Right CASE
         return getLeftRotate(root);
     }
 
-    // Left-Right CASE
-    if (balance > 1 && getBalance(root->left) < 0) {
+    if (balance > 1 && getBalance(root->left) < 0) {    // Left-Right CASE
         return getDoubleRightRotate(root);
     }
 
-    // Right-Left CASE
-    if (balance < -1 && getBalance(root->right) > 0) {
+    if (balance < -1 && getBalance(root->right) > 0) {  // Right-Left CASE
         return getDoubleLeftRotate(root);
     }
-
     return root;
 }
